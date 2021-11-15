a5fa52a19260329af835ea98bda02e0be2a76d04
duajaradat <duajaradat164@gmail.com>
2021-11-15 23:39:31 +0200

    feat : project with one model

--- snacks/models.py
+++ snacks/models.py
@@ -4,3 +4,6 @@ class Snack(models.Model):
     name = models.CharField(max_length=64)
     purchaser = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     description = models.TextField(default=0)
+
+    def __str__(self):
+        return self.name
